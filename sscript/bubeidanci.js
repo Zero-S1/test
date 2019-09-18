8e0cb58c08769b3435d2bafd29dd6ab153078389
supaL <23739314+haiBosuPal@users.noreply.github.com>
2019-09-18 21:52:28 +0800

    Create bubeidanci.js

---
+++ sscript/bubeidanci.js
@@ -0,0 +1,25 @@
+let body = $response.body
+
+body=JSON.parse(body)
+
+
+
+body["data_body"]["privileges"]["collins"]["granted"]=1
+body["data_body"]["privileges"]["collins"]["collins_user_type"]=1
+
+
+body["data_body"]["privileges"]["wordroot"]["granted"]=1
+body["data_body"]["privileges"]["wordroot"]["user_type"]=1
+
+
+
+
+body["data_body"]["privileges"]["collins"]["expire_date"]=1702449935
+body["data_body"]["privileges"]["wordroot"]["expire_date"]=1702449935
+
+body["data_body"]["user_status"]["dl_quota"]=935
+
+
+
+body=JSON.stringify(body)
+$done({body})
