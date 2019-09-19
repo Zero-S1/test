861cfe07cf3853007adca041fd3f34273a9163a7
supaL <23739314+haiBosuPal@users.noreply.github.com>
2019-09-19 12:24:39 +0800

    Update and rename baicizhanreader to baicizhanreader.js

--- sscript/baicizhanreader.js
+++ sscript/baicizhanreader.js
@@ -2,10 +2,12 @@ let body = $response.body
 body=JSON.parse(body)
 
 if(body['scholar_member_remain']){
-body['scholar_member_remain']=1}
+body['scholar_member_remain']=1
+}
 
 if(body['membership_remains']){
-body['membership_remains']=666}
+body['membership_remains']=666
+}
 
 
 body=JSON.stringify(body)
