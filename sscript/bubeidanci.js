ddbe2a1112137f9b40159d35c57cf2bc97574642
supaL <23739314+haiBosuPal@users.noreply.github.com>
2019-09-19 15:42:56 +0800

    Update bubeidanci.js

--- sscript/bubeidanci.js
+++ sscript/bubeidanci.js
@@ -1,4 +1,4 @@
-let body = $response.body
+let body1 = $response.body
 
 
 /*
@@ -12,10 +12,10 @@ body["data_body"]["privileges"]["wordroot"]["expire_date"]=1702449935
 body=JSON.stringify(body)
 */
 //body=body.string
-body=body.replace(/\"granted\":\d/g,"\"granted\":1")
-body=body.replace(/\"expire_date\":\d+/g,"\"expire_date\":1702449935")
-body=body.replace(/\"collins_user_type\":\d/g,"\"collins_user_type\":1")
-body=body.replace(/\"user_type\":\d/g,"\"user_type\":1")
+body1=body1.replace(/\"granted\":\d/g,"\"granted\":1")
+body1=body1.replace(/\"expire_date\":\d+/g,"\"expire_date\":1702449935")
+body1=body1.replace(/\"collins_user_type\":\d/g,"\"collins_user_type\":1")
+body1=body1.replace(/\"user_type\":\d/g,"\"user_type\":1")
 //body=JSON.parse(body)
 //body=JSON.stringify(body)
 
@@ -24,6 +24,6 @@ body=body.replace(/\"user_type\":\d/g,"\"user_type\":1")
 
 var title = "supaL与您相伴";
 var subtitle = "天气状况："
-var mation = body
+var mation = body1
 $notification.post(title, subtitle, mation);
 $done();
