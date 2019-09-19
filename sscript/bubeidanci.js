326a0b6ef095f8dd93249a8c1df48dbb18dce9a1
supaL <23739314+haiBosuPal@users.noreply.github.com>
2019-09-19 15:58:05 +0800

    Update bubeidanci.js

--- sscript/bubeidanci.js
+++ sscript/bubeidanci.js
@@ -11,16 +11,15 @@ body["data_body"]["privileges"]["collins"]["expire_date"]=1702449935
 body["data_body"]["privileges"]["wordroot"]["expire_date"]=1702449935
 body=JSON.stringify(body)
 
-
+*/
 body1=body1.replace(/\"granted\":\d/g,"\"granted\":1")
 body1=body1.replace(/\"expire_date\":\d+/g,"\"expire_date\":1702449935")
 body1=body1.replace(/\"collins_user_type\":\d/g,"\"collins_user_type\":1")
 body1=body1.replace(/\"user_type\":\d/g,"\"user_type\":1")
-//body=JSON.parse(body)
-//body=JSON.stringify(body)
+
 
 //$done({body})
-*/
+
 
 var title = "supaL与您相伴";
 var subtitle = "天气qq"
