5e37207841c0f0aeef403c912837ad7d401a43ae
supaL <23739314+haiBosuPal@users.noreply.github.com>
2019-09-19 15:52:00 +0800

    Update bubeidanci.js

--- sscript/bubeidanci.js
+++ sscript/bubeidanci.js
@@ -10,8 +10,8 @@ body["data_body"]["privileges"]["wordroot"]["user_type"]=1
 body["data_body"]["privileges"]["collins"]["expire_date"]=1702449935
 body["data_body"]["privileges"]["wordroot"]["expire_date"]=1702449935
 body=JSON.stringify(body)
-*/
-//body=body.string
+
+
 body1=body1.replace(/\"granted\":\d/g,"\"granted\":1")
 body1=body1.replace(/\"expire_date\":\d+/g,"\"expire_date\":1702449935")
 body1=body1.replace(/\"collins_user_type\":\d/g,"\"collins_user_type\":1")
@@ -20,10 +20,10 @@ body1=body1.replace(/\"user_type\":\d/g,"\"user_type\":1")
 //body=JSON.stringify(body)
 
 //$done({body})
-
+*/
 
 var title = "supaL与您相伴";
 var subtitle = "天气qq"
-var mation = body
+var mation = body1
 $notification.post(title, subtitle, mation);
 $done();
