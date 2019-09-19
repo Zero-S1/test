5844b31845947387cbcef7897b18c99e373ab965
supaL <23739314+haiBosuPal@users.noreply.github.com>
2019-09-19 16:10:53 +0800

    Update bubeidanci.js

--- sscript/bubeidanci.js
+++ sscript/bubeidanci.js
@@ -14,10 +14,10 @@ body=JSON.stringify(body)
 */
 
 ff=body1.search(/granted\":\d/g)
-body1=body1.replace(/granted\\\":\d/g,"granted\\\":1")
-body1=body1.replace(/expire_date\\\":\d+/g,"expire_date\\\":1702449935")
-body1=body1.replace(/collins_user_type\\\":\d/g,"collins_user_type\\\":1")
-body1=body1.replace(/"user_type\\\":\d/g,"user_type\\\":1")
+body1=body1.replace(/granted\":\d/g,"granted\":1")
+body1=body1.replace(/expire_date\":\d+/g,"expire_date\":1702449935")
+body1=body1.replace(/collins_user_type\":\d/g,"collins_user_type\":1")
+body1=body1.replace(/"user_type\":\d/g,"user_type\":1")
 
 
 //$done({body})
