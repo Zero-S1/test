7c9cb81bdd6ec01eff18667361f6067af38b3bbf
supaL <23739314+haiBosuPal@users.noreply.github.com>
2019-09-19 16:28:52 +0800

    Update bubeidanci.js

--- sscript/bubeidanci.js
+++ sscript/bubeidanci.js
@@ -11,20 +11,20 @@ body["data_body"]["privileges"]["collins"]["expire_date"]=1702449935
 body["data_body"]["privileges"]["wordroot"]["expire_date"]=1702449935
 body=JSON.stringify(body)
 
-*/
 
-ff=body1.search(/granted":\d/g)
+
+ff=body1.search(/granted\":\d/g)
 body1=body1.replace(/granted\":\d/g,"granted\":1")
 body1=body1.replace(/expire_date\":\d+/g,"expire_date\":1702449935")
 body1=body1.replace(/collins_user_type\":\d/g,"collins_user_type\":1")
 body1=body1.replace(/"user_type\":\d/g,"user_type\":1")
-
+*/
 
 //$done({body})
 
 
-var title = $request.url;
-var subtitle = ff
+var title = (new Date()).valueOf();
+var subtitle = type(body1)
 var mation = body1
 $notification.post(title, subtitle, mation);
 
